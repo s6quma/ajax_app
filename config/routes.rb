5af558a7ad8d0b689e63555487be34beaeed3364
s6quma <s6quma@yahoo.co.jp>
2021-01-21 20:00:51 +0900

    既読機能実装
    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   #HTTPメソッド 'URIパターン', to: 'コントローラー名#アクション名'
   root to: 'posts#index'
   post 'posts', to:'posts#create'
+  get 'post/:id', to: 'post#checked'
   end
 

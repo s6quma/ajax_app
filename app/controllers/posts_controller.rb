5b52f8f1ffe21887ab17067d8de10dcb9ad3211d
s6quma <s6quma@yahoo.co.jp>
2021-01-21 19:01:40 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,14 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
-    #@post = Post.find(1) 1番目のレコードを@postに代入
+    @posts = Post.all.order(id: "DESC")
   end
+
   def new
   end
+
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
